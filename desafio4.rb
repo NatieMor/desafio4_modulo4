1b5c83f903168b25ad9d766d2da9d9be539bb9f2
Nathalie Moraga <129231844+NatieMor@users.noreply.github.com>
2023-06-05 12:38:13 -0400

    update desafio4.rb
    falta nombre de cámara en el requerimiento 3

--- desafio4.rb
+++ desafio4.rb
@@ -2,6 +2,7 @@ require 'uri'
 require 'net/http'
 require 'json'
 
+#REQUERIMIENTO1
 def request(url_requested)
     url = URI(url_requested)
 
@@ -12,15 +13,70 @@ def request(url_requested)
     request["cache-control"] = 'no-cache'
     request["postman-token"] = '5f4b1b36-5bcd-4c49-f578-75a752af8fd5'
     response = http.request(request)
-    data_hash=JSON.parse(response.body) #en esta parte el requerimiento (request) se retorna como un HASH 
-end
-data_hash= request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=C6d6ya1SRztiqEpUbmTMl08Btkeefy7NuRDvt4Gf')
- puts data_hash["photos"]#se imprime el retorno del request como un HASH, atraves del uso de la key, se imprime el valor de la key "photos"
+    return JSON.parse(response.body) #en esta parte el requerimiento (request) se retorna como un HASH 
+end 
+$data= request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=C6d6ya1SRztiqEpUbmTMl08Btkeefy7NuRDvt4Gf")
+puts "Se retorno un #{$data.class} con los siguientes datos (valores): " #aqui comprobamos que nos devolvio un HASH
+puts
+puts $data.values #conocemos cuales son los valores del HASH
+puts #Aqui conocemos la keys, la clase que lo conforma y el nombre de las key
+puts "La key del hash es: #{$data.keys} , que esta formando un #{$data["photos"].class} compuesto por las siguientes KEYS #{$data["photos"][0].keys}" #conocemos cuales son las keys
+puts 
 
- puts data_hash["photos"][0..10]
- #tiene que ser un ciclo..
- data_hash["photos"].each do |elemento|
-               puts "la id es #{data_hash["photos"][0]["id"]} y la imagen es #{data_hash["photos"][0]["img_src"]}"
-      puts
-end
+#REQUERIENTO 2
+def buid_web_page(data)
+  html="<html>
+  <meta charset='utf-8'/>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
+  <meta name='copyright' content='derechos reservados. Proibida su reproducción' />
+  <meta name='author' content='Nathalie Moraga'/>
+  <meta name='description' content='imagenes nasa'/>
+  <meta name='keyword' content='espacio, nacio, sol, rover, imagenes, ciencia'/>
+  <link rel='shortcut icon' href='x'/>
+  <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css'
+    integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/'crossorigin='anonymous'/>
+  <link rel='stylesheet'type='text/css' href='asset/css/Style.css' />
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css'
+    integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'/>
+  <link href='https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Playfair+Display&display=swap'
+    rel='stylesheet'>
+   <title>Imagenes del sol NASA</title>
+  </head>
+  <body>
+  <div class='container-fluid text-center'>
+    <h1 class='display-1 text-center'>Imagenes NASA </h1>
+    <div class='row col-6 d-inline'>
+    "
+    $data["photos"].each do |elemento|
+    elemento["id"]
+    elemento["img_src"]
+    elemento["earth_date"]
+     #print "ID de la imagen: #{elemento["id"]} y su link: #{elemento["img_src"]}" 
+     html << "<ul class='mb-3'>
+     <div class='card container col-12 mb-2'>
+     <img src=' #{elemento["img_src"]}' class='card-img-bottom' alt='#{elemento["id"]}' title='#{elemento["earth_date"]}'>
+     <div class='card-body'>
+    <h2 class='card-title display-6 text-success'> Imagen # #{elemento["id"]}</h2>
+    </div>
+  </div>
+  </ul>"
+  end
+  html << "
+  </div>
+  </div>
+  <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js' integrity='sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz' crossorigin='anonymous'></script>
+  </body>
+  </html>"
+  File.write('index.html', html)
+  end
+  buid_web_page($data)
 
+  #REQUERIENTO 3
+  def photos_count(data)
+    i = 0
+    $data["photos"].each do |elemento|
+        i += 1
+    end
+    puts "Tenemos #{i} Imagenes de la NASA"
+  end
+  photos_count($data)
